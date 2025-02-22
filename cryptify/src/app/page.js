6ad8791767ee434c1205019497dfb2c09f7e83aa
danoruo1 <138059604+danoruo1@users.noreply.github.com>
2025-02-22 17:10:34 -0500

    Update page.js

--- cryptify/src/app/page.js
+++ cryptify/src/app/page.js
@@ -2,7 +2,7 @@
 import {Box, Typography,Button, Container} from "@mui/material"
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import '../src/app/custom.css';  // Adjust the path to where custom.css is located
+import '/src/app/custom.css';  // Adjust the path to where custom.css is located
 
 export default function Home() {
   const router = useRouter();
