c88c8b79cf0b7e2f31b645af8124f8bc65b63d91
danoruo1 <138059604+danoruo1@users.noreply.github.com>
2025-02-23 17:44:35 -0500

    s

--- cryptify/src/app/help/page.js
+++ cryptify/src/app/help/page.js
@@ -49,16 +49,54 @@ export default function Home() {
         style={{
           background:"transparent",
           width:"80%",
-          height:"25%",
+          height:"45%",
           gap:"20%",
           overflow:"clip",
           position:"relative",
           alignItems:"center",
           display:"flex",
           position:"relative",
-          top:"-100px"
+          top:"-250px"
         }}
       >
+
+
+
+      <Box
+          sx={{
+            width: "100%",
+            backgroundColor: "black",
+            color: "white",
+            fontFamily: "glitchFont",
+            height: "100%",
+            borderRadius: "20px",
+            border: "7px solid green",
+            fontSize: "25px",
+            overflow: "auto"
+        }}>
+          <Typography sx={{
+            fontFamily:"glitchFont",
+            fontSize:"50px",
+            color:"white",
+            position:"relative",
+            }}> Encryption <br></br>
+            *Utilizing different Cryptographic Algorithms, you can encode your message. All you have to do is follow the given instructions during the encryption process, and then make sure to enter the right userID(if your sending a message). 
+            <br></br>
+            <br></br>
+            Decryption <br></br>
+            *Either you can import a json file and decrypt the message from the database, or create a session between another user to send a message.
+            <br></br>
+
+
+            <br></br>
+            Posting to the Database
+            <br></br>
+            *Please ensure that all posted content is appropriate and free of any profanity.
+          </Typography>
+
+
+
+        </Box>
         
 
       </div>
@@ -71,7 +109,7 @@ export default function Home() {
             alignItems: "center",
             justifyContent: "center",
             position: "relative",
-            top: "-100px"
+            top: "-320px"
             }}>
           <Button className="startButton" onClick={() => handleRedirect('/')}> Back </Button>
       </footer>
